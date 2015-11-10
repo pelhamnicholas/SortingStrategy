ae38208861d1e3fecd8da5a8ac1cef26bb877b5c
Nicholas Pelham <npelh001@hammer.cs.ucr.edu>
2015-11-10 13:03:29 -0800

    combined sortalgorithm.h into sontainer.h to fix compiler errors

--- src/container.h
+++ src/container.h
@@ -1,21 +1,32 @@
 #ifndef CONTAINER_H
 #define CONATAINER_H
 
-#include "sortalgorithm.h"
+class SortAlgorithm;
 
 class Container {
     private:
 
+    protected:
+        SortAlgorithm * sortAlgo;
+
     public:
         Container() {};
-        ~Container() {};
-        virtual int at(int ) {} = 0;
-        virtual void swap(int , int) {} = 0;
-        virtual void insert(int) {} = 0;
-        virtual void print() {} = 0;
-        virtual int size() {} = 0;
-        virtual void set_sort(SortAlgorithm *) {} = 0;
-        virtual void sort() {} = 0;
+        virtual ~Container() {};
+        virtual int at(int ) = 0;
+        virtual void swap(int , int) = 0;
+        virtual void insert(int) = 0;
+        virtual void print() = 0;
+        virtual int size() = 0;
+        virtual void set_sort(SortAlgorithm *) = 0;
+        virtual void sort() = 0;
+};
+
+class SortAlgorithm {
+    private:
+    public:
+        SortAlgorithm() {};
+        ~SortAlgorithm() {};
+        virtual void sort(Container *) = 0;
 };
 
 #endif
